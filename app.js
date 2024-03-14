46025b0857c35e3c33303ab4b17e639a3b55f2ec
sahzadahmad246 <124631079+sahzadahmad246@users.noreply.github.com>
2024-03-14 23:56:34 +0530

    commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const router = require('./routes/router');
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "https://sonipaintingworks.onrender.com/"],
   methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "FETCH"],
   credentials: true,
 };
