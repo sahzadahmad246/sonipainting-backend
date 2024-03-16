4d60f01bc553979f4ec0f481209db113fbd5d0fb
sahzadahmad246 <124631079+sahzadahmad246@users.noreply.github.com>
2024-03-16 22:50:24 +0530

    commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "https://sonipaintingworks.onrender.com",
-    "https://sonipainting.com/",
+    "https://sonipainting.com",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "FETCH"],
   credentials: true,
