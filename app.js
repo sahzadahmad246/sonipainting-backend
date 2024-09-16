62707179624d7e132819f707776881dcbd3581a4
Shahzad Ahmad <sahzadahmad246@gmail.com>
2024-09-16 15:09:39 +0530

    hi

--- app.js
+++ app.js
@@ -40,7 +40,9 @@ app.use(
     cookie: {
       maxAge: 6 * 30 * 24 * 60 * 60 * 1000,
       secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     },
+    
   })
 );
 
